fae4ba53eabe14264d552042bb256d66c7bc75ee
Vitesco Technologies GmbH <54802215+vitesco-io@users.noreply.github.com>
2020-11-03 17:22:48 +0100

    Create mirror.sh

---
+++ mirror.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git config --global credential.helper '!aws codecommit credential-helper $@' 
+git config --global credential.UseHttpPath true
+
+aws secretsmanager get-secret-value --secret-id $SECRET_NAME | jq -r '.SecretString' > /tmp/key && chmod 400 /tmp/key
+
+GIT_SSH_COMMAND="ssh -i /tmp/key -o StrictHostKeyChecking=no " git clone --mirror $SOURCE_REPO source
+cd source && git push --mirror $TARGET_REPO
